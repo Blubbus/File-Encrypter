6133a95aa402247d7b1a2a005ab3ff6b9f42bbde
Ole Liebich <blubbbus@gmx.net>
2020-05-13 16:14:54 +0200

    Cleaned code up

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from pathlib import Path
+import logging
 
+def encryptFile(filePath, password):
+    hashObj = SHA256.new(password.encode('utf-8'))
+    hkey = hashObj.digest()
+    with filePath.open("rb") as input_file, open(filePath.resolve().as_posix() +".enc", "wb") as output_file:
+        content = ""
+        for byte in input_file.read():
+            content += str(byte) + " "
+        content = content[:-1]
+        encoded = encrypt(hkey, content)
+        output_file.write(encoded)
+        logging.info("Encoded " + filePath.resolve().as_posix())
 
+def decryptFile(filePath, password):
+    hashObj = SHA256.new(password.encode('utf-8'))
+    hkey = hashObj.digest()
+    with filePath.open("rb") as input_file, open(filePath.resolve().as_posix()[:-4], "wb") as output_file:
+        decoded = decrypt(hkey, input_file.read())
+        content = b''
+
+        for byte in decoded.split():
+
+            content += int(byte).to_bytes(1, 'big')
+        output_file.write(content)
+        
+        logging.info("Decoded: " + filePath.resolve().as_posix()[:-4])
 
 def pad(msg, BLOCK_SIZE, PAD):
     return msg.encode('utf-8') + PAD * (BLOCK_SIZE - len(msg) % BLOCK_SIZE)
@@ -20,8 +45,6 @@ def decrypt(key, msg):
     decipher = AES.new(key, AES.MODE_ECB)
     pt = decipher.decrypt(msg).decode('utf-8')
     for i in range(len(pt)-1, -1, -1):
-        if pt[i].replace(" ", "") == "":
-            continue
         if pt[i].encode('utf-8') == PAD:
            # print("HIT")
             pt = pt[:i]
@@ -37,56 +60,44 @@ def getMaxLen(arr):
             maxLen = len(elem)
     return maxLen
 
+def getTargetFiles(fileExtension):
+    fileExtensionFormatted = "*."
+    if len(fileExtension) == 0:
+        fileExtensionFormatted = "*"
+    for i in range(0, getMaxLen(fileExtension)):
+        formatted = "["
+        for extension in  fileExtension:
+            if len(extension) > i:
+                formatted += extension[i]
+        formatted += "]"
+        fileExtensionFormatted += formatted
+    return fileExtensionFormatted
+
 if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
     print("(1) - encrypt\n(2) - decrypt")
     mode = int(input("---> "))
-    if mode == 1:
-        password = input("password: ")
-        passwordConfirm = input("confirm password: ")
-        fileExtension = input("Enter file extensions (jpg png ...): ").split()
-        fileExtensionFormatted = "*."
-        for i in range(0, getMaxLen(fileExtension)):
-            formatted = "["
-            for extension in  fileExtension:
-                if len(extension) > i:
-                    formatted += extension[i]
-            formatted += "]"
-            fileExtensionFormatted += formatted
-        print("Using " + fileExtensionFormatted)
-        if password == passwordConfirm:
-            hashObj = SHA256.new(password.encode('utf-8'))
-            hkey = hashObj.digest()
-            result = list(Path(".").rglob(fileExtensionFormatted))
-            for paths in result:
-                with paths.open("rb") as input_file, open(paths.resolve().as_posix() +".enc", "wb") as output_file:
-                    content = ""
-                    for byte in input_file.read():
-                        content += str(byte) + " "
-                    content = content[:-1]
-                    encoded = encrypt(hkey, content)
-                    output_file.write(encoded)
-                    print("Encoded " + paths.resolve().as_posix())
-        else:
-            print("Passwords not matching!")
-            exit()
+    password = input("password: ")
+    passwordConfirm = input("confirm password: ")
+    if password != passwordConfirm:
+        logging.error("Passwords not matching")
+        exit()
     
+
+    if mode == 1:
+        fileExtensions = input("Enter file extensions (jpg png ...): ").split()
+        fileExtensionFormatted = getTargetFiles(fileExtensions)
+        logging.debug("Using " + fileExtensionFormatted)
+        filePaths = list(Path(".").rglob(fileExtensionFormatted))
+        for filePath in filePaths:
+            encryptFile(filePath, password)
+        
     elif mode == 2:
-        password = input("password: ")
-        passwordConfirm = input("confirm password: ")
-        if password == passwordConfirm:
-            hashObj = SHA256.new(password.encode('utf-8'))
-            hkey = hashObj.digest()
-            result = list(Path(".").rglob("*.[eE][nN][cC]"))
-            for paths in result:
-                with paths.open("rb") as input_file, open(paths.resolve().as_posix()[:-4], "wb") as output_file:
-                    decoded = decrypt(hkey, input_file.read())
-                    content = b''
-                    #print(decoded)
-                    #print("Hier wars")
-                    for byte in decoded.split():
-                        #print("Byte: ", end="")
-                        #print(byte)
-                        content += int(byte).to_bytes(1, 'big')
-                    output_file.write(content)
-                    print("Decoded: " + paths.resolve().as_posix()[:-4])
+        filePaths = list(Path(".").rglob("*.[eE][nN][cC]"))
+        
+        for filePath in filePaths:
+            print(filePath.resolve().as_posix())
+            decryptFile(filePath, password)
 
